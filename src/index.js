75f7dd81b3dbb3a69f7a20065e6ca1a192a75403
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-03-07 21:40:28 +0200

    update v4

--- src/index.js
+++ src/index.js
@@ -16,10 +16,13 @@ dotenv.config();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173', 'http://localhost:5174', 'https://dako23.web.app'],
-        credentials: true,
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Authorization"],
+        credentials: true
     }
 });
+
 
 initSocket(io);
 
