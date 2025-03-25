30dceb0811cb94c11434bc6231b389856013bfdc
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-03-25 12:24:00 +0200

    update

--- src/socket.js
+++ src/socket.js
@@ -25,10 +25,10 @@ export default function initSocket(io) {
         });
 
         // 🔹 Изпращане на съобщение
-        socket.on("sendMessage", async ({ groupId, senderId, message }) => {
+        socket.on("sendMessage", async ({ groupId, senderId, message, username, imageUrl }) => {
             try {
                 // 📌 Запазване на съобщението в базата
-                const newMessage = await chatService.saveMessage(groupId, senderId, message);
+                const newMessage = await chatService.saveMessage(groupId, senderId, message, username, imageUrl);
 
                 // 📌 Изпращане на съобщението на всички в групата
                 io.to(groupId).emit("receiveMessage", newMessage);
