ad4149c3fe90032675be3dfd177d1e105cd73cfb
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-03-31 10:18:09 +0300

    update

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ app.use(cors({
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT,PATCH, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Set-Cookie");
     res.setHeader("Access-Control-Expose-Headers", "Set-Cookie");
     res.setHeader("P3P", 'CP="This site does not have a P3P policy"');
