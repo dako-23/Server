496f239029b50bd6710139edd335e061ef58dadc
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-04-16 13:51:49 +0300

    update

--- src/controllers/newStyleGardenGallery.js
+++ src/controllers/newStyleGardenGallery.js
@@ -5,34 +5,18 @@ const galleryController = Router();
 
 galleryController.get('/', async (req, res) => {
 
-    const shouldFix = req.query.fix === "true"; // ?fix=true
-
     try {
         const result = await cloudinary.api.resources({
             type: "upload",
+            prefix: "gallery/",
             max_results: 100,
         });
 
-        const images = [];
-
-        for (const asset of result.resources) {
-            const isInGallery = asset.public_id.startsWith("gallery/");
-
-            if (shouldFix && !isInGallery) {
-                const newPublicId = `gallery/${asset.public_id}`;
-                try {
-                    await cloudinary.uploader.rename(asset.public_id, newPublicId);
-
-                    images.push(`https://res.cloudinary.com/${cloudinary.config().cloud_name}/image/upload/${newPublicId}.${asset.format}`);
-                } catch (renameErr) {
-                }
-            } else {
-                images.push(asset.secure_url);
-            }
-        }
+        const images = result.resources.map(img => img.secure_url);
 
         res.json(images);
     } catch (err) {
+        console.error("Cloudinary error:", err.message);
         res.status(500).json({ error: "Failed to load images" });
     }
 
