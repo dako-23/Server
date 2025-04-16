5f418818bfe2203741f766d368b6e904e9426626
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-04-16 15:45:22 +0300

    update

--- src/controllers/newStyleGardenGallery.js
+++ src/controllers/newStyleGardenGallery.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router } from 'express';
 import cloudinary from '../config/cloudinary.js';
 
 const galleryController = Router();
@@ -7,20 +7,66 @@ galleryController.get('/', async (req, res) => {
 
     try {
         const result = await cloudinary.api.resources({
-            type: "upload",
-            prefix: "gallery/",
+            type: 'upload',
+            prefix: 'gallery/',
             max_results: 100,
+            direction: 'desc',
+            sort_by: [{ created_at: 'desc' }]
         });
 
         const images = result.resources.map(img => img.secure_url);
 
         res.json(images);
     } catch (err) {
-        console.error("Cloudinary error:", err.message);
-        res.status(500).json({ error: "Failed to load images" });
+        console.error('Cloudinary error:', err.message);
+        res.status(500).json({ error: 'Failed to load images' });
     }
 
 })
+
+galleryController.get('/carousel', async (req, res) => {
+
+    try {
+        const result = await cloudinary.api.resources({
+            type: 'upload',
+            prefix: 'home-page/',
+            max_results: 5,
+            direction: 'desc',
+            sort_by: [{ created_at: 'desc' }]
+        });
+
+        const images = result.resources.map(img => img.secure_url);
+
+        res.json(images);
+    } catch (err) {
+        console.error('Cloudinary error:', err.message);
+        res.status(500).json({ error: 'Failed to load images' });
+    }
+
+})
+
+galleryController.get('/home-slider', async (req, res) => {
+
+    try {
+        const result = await cloudinary.api.resources({
+            type: 'upload',
+            prefix: 'gallery/',
+            max_results: 15,
+            direction: 'desc',
+            sort_by: [{ created_at: 'desc' }]
+        });
+
+        const images = result.resources.map(img => img.secure_url);
+
+        res.json(images);
+    } catch (err) {
+        console.error('Cloudinary error:', err.message);
+        res.status(500).json({ error: 'Failed to load images' });
+    }
+
+})
+
+
 
 
 export default galleryController
